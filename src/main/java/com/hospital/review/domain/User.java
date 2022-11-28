7d7b11d019761a175ae58731a8be614777428fe6
soonmineom <skdlfma123@gmail.com>
2022-11-28 14:01:02 +0900

    User Join기능 추가, 중복 검사 후 저장까지

--- src/main/java/com/hospital/review/domain/User.java
+++ src/main/java/com/hospital/review/domain/User.java
@@ -1,15 +1,23 @@
 package com.hospital.review.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String userName;
     private String password;
     private String emailAddress;
--- src/main/java/com/hospital/review/service/UserService.java
+++ src/main/java/com/hospital/review/service/UserService.java
@@ -1,13 +1,32 @@
 package com.hospital.review.service;
 
+import com.hospital.review.domain.User;
 import com.hospital.review.domain.dto.UserDto;
 import com.hospital.review.domain.dto.UserJoinRequest;
+import com.hospital.review.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
     public UserDto join(UserJoinRequest request) {
-        return new UserDto("","","");
+//        // 검색한 유저 네임이 없을 때.
+//        userRepository.findByUserName(request.getUserName())
+//                .orElseThrow(()->new RuntimeException("해당 UserName이 존재하지 않습니다."));
+
+        //중복체크 = 검색한 유저 네임이 있을 때.
+        userRepository.findByUserName(request.getUserName())
+                .ifPresent(user->new RuntimeException("이미 존재하는 UserName 입니다."));
+        //회원가입
+        User savedUser = userRepository.save(request.toEntity());
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .email(savedUser.getEmailAddress())
+                .build();
     }
 }
